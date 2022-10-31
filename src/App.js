488086e2ad2ceab1d7fa09c71b2380903aa0a893
earthssu <entjq9502@khu.ac.kr>
2022-10-31 21:13:17 +0900

    FIX: editor ref 적용 확인

--- src/App.js
+++ src/App.js
@@ -4,38 +4,51 @@ import 'tui-image-editor/dist/tui-image-editor.css';
 import ImageEditor from '@toast-ui/react-image-editor';
 
 function App() {
+  const editorRef = React.createRef();
+
+  const handleClickButton = () => {
+    const editorInstance = editorRef.current.getInstance();
+    editorInstance.flipX();
+  };
+
   return (
-    <ImageEditor
-      includeUI={{
-        loadImage: {
-          name: 'SampleImage',
-        },
-        menu: [
-          'crop',
-          'flip',
-          'rotate',
-          'draw',
-          'shape',
-          'icon',
-          'text',
-          'mask',
-          'filter',
-        ],
-        initMenu: 'filter',
-        uiSize: {
-          width: '1000px',
-          height: '700px',
-        },
-        menuBarPosition: 'right',
-      }}
-      cssMaxHeight={500}
-      cssMaxWidth={700}
-      selectionStyle={{
-        cornerSize: 20,
-        rotatingPointOffset: 70,
-      }}
-      usageStatistics
-    />
+    <>
+      <ImageEditor
+        ref={editorRef}
+        includeUI={{
+          loadImage: {
+            name: 'SampleImage',
+          },
+          menu: [
+            'crop',
+            'flip',
+            'rotate',
+            'draw',
+            'shape',
+            'icon',
+            'text',
+            'mask',
+            'filter',
+          ],
+          initMenu: 'filter',
+          uiSize: {
+            width: '100%',
+            height: '700px',
+          },
+          menuBarPosition: 'right',
+        }}
+        cssMaxHeight={500}
+        cssMaxWidth={700}
+        selectionStyle={{
+          cornerSize: 20,
+          rotatingPointOffset: 70,
+        }}
+        usageStatistics
+      />
+      <button onClick={handleClickButton} type="button">
+        Flip image by X Axis!
+      </button>
+    </>
   );
 }
 
